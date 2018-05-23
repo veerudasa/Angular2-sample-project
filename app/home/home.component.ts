e086755ddd0bd82b94ff917f417343baced40357
Veeru <veeru.dasa@gmail.com>
2018-05-22 23:27:15 -0700

    First commit

---
+++ app/home/home.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {StateService} from '../common/state.service';
+
+@Component({
+  selector: 'home',
+  template: require('./home.component.html')
+})
+export class HomeComponent implements OnInit {
+  title: string = 'Home Page';
+  body:  string = 'This is About Home body';
+  message: string;
+
+  constructor(private _stateService: StateService) { }
+
+  ngOnInit() {
+    this.message = this._stateService.getMessage();
+  }
+
+  updateMessage(m: string): void {
+    this._stateService.setMessage(m);
+  }
+}
